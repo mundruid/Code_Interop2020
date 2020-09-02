ca1dde3e50f74ff3526d46ddf08700eea125127e
Mr. Alheid & Dr. X et. al <mountrouidoux@cofc.edu>
2020-09-02 17:58:38 -0400

    mock mock examples

--- examples/mock.py
+++ examples/mock.py
@@ -1,2 +1,15 @@
+mock.patch('package.module.Classname')
 
-# def validate_mac_address(mac):
+mock.patch('package.module.Classname', return_value='ping')
+
+mock.patch('package.module.Classname', side_effect='pong')
+
+@mock.patch('package.module.Classname', return_value='echo')
+def test_example(self, m):
+
+@mock.patch('package.module.Classname', return_value='echo')
+class TestExample:
+    def test_example(self, m):
+
+with @mock.patch('package.module.Classname', return_value='echo') as m:
+
